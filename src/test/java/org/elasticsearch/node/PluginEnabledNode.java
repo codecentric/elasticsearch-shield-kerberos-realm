d7ab67fb10f817285f6e3102864f908264afacd8
Hendrik Saly <hendrikdev22@gmail.com>
2016-02-16 12:40:27 -0800

    ES version 2.1.1

--- src/test/java/org/elasticsearch/node/PluginEnabledNode.java
+++ src/test/java/org/elasticsearch/node/PluginEnabledNode.java
@@ -5,14 +5,12 @@ import java.util.Collection;
 import org.elasticsearch.Version;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.node.Node;
+import org.elasticsearch.node.internal.InternalSettingsPreparer;
 import org.elasticsearch.plugins.Plugin;
 
 public class PluginEnabledNode extends Node{
 
     public PluginEnabledNode(Settings preparedSettings, Collection<Class<? extends Plugin>> classpathPlugins) {
-        super(preparedSettings, Version.CURRENT, classpathPlugins);
+        super(InternalSettingsPreparer.prepareEnvironment(preparedSettings, null), Version.CURRENT, classpathPlugins);
     }
-
-    
-    
 }
